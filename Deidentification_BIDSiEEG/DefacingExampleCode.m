a00e8c774d5e28fc1b714502fb7a01202896d904
Angelique C Paulk <APaulk@users.noreply.github.com>
2022-09-18 20:12:28 -0400

    Update DefacingExampleCode.m

--- Deidentification_BIDSiEEG/DefacingExampleCode.m
+++ Deidentification_BIDSiEEG/DefacingExampleCode.m
@@ -1,10 +1,9 @@
-
 %% Sets up the directories and the files to de-identify/deface
 clear 
 addpath(genpath('X:\Projects\Lab_Materials\Analysis_Tools_and_Software\fieldtrip-20220202\'))
 
-PatientName='sub-0d9l';
-wd = ['Y:\StimDataBackup\Data_Stimulation\DeIdentifiedDataSet\'];
+PatientName='sub-5o1r';
+wd = ['Y:\ReconPipelinePaper\Data\'];
 
 DirVal = fullfile(wd , ['derivatives\freesurfer\',PatientName], 'surf'); %the surf folder with the freesurfer output
 RASExcelDirectory = fullfile(wd, PatientName, 'ses-postimp','ieeg' );
@@ -19,7 +18,7 @@ clear post_anon mri mri_anon post
 Desig=PatientName;
 Desig
 %% Reads the preoperative MRI
-mri = ft_read_mri([PreopMRIDirectory,PreopMRIFile(1).name]);
+mri = ft_read_mri([PreopMRIDirectory,'\',PreopMRIFile(1).name]);
 %
 cfg = [];
 mri_anon = ft_defacevolume(cfg, mri);
@@ -27,10 +26,9 @@ mri_anon = ft_defacevolume(cfg, mri);
 ft_write_mri([PreopMRIDirectory,'\',Desig,'_anon.nii'], mri_anon.anatomy, 'transform', mri_anon.transform, 'dataformat', 'nifti');
 
 %% Reads the postoperative scan (CT or MRI)
-post = ft_read_mri([PostopMRIDirectory,PostopMRIFile(1).name]);
+post = ft_read_mri([PostopMRIDirectory,'\',PostopMRIFile(1).name]);
 %
 cfg = [];
-post_anon = ft_defacevolume(cfg, ct);
+post_anon = ft_defacevolume(cfg, post);
 %
 ft_write_mri([PostopMRIDirectory,'\',Desig,'_anonPostOPScan.nii'], post_anon.anatomy, 'transform', post_anon.transform, 'dataformat', 'nifti');
-NUMCh
