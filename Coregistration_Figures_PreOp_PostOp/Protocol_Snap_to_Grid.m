967a3c55cf889ec8c96d5a6d18812c57ad34a659
djsoper <44448586+djsoper@users.noreply.github.com>
2022-11-29 19:21:06 -0500

    Uploaded Updated Snap2grid

--- Coregistration_Figures_PreOp_PostOp/Protocol_Snap_to_Grid.m
+++ Coregistration_Figures_PreOp_PostOp/Protocol_Snap_to_Grid.m
@@ -1,7 +1,15 @@
-%% Load the inputs for the snap2surf file
-Pt = 'id##';
-ElectrodeRASPath = fullfile( '/home/bourbon-the-huckster/Documents/Recons', Pt, [Pt '_RAS.xlsx']); 
-[num,txt,all]=xlsread(ElectrodeRASPath);
+%% Modular Reconstruction and Co-registration of Imaging from Implanted ECoG and SEEG Electrodes
+%
+% Code for Snapping Grids and Strips to Pial surface
+%
+% This version was completed on November 29th, 2022
+% 
+% For questions, please post issues to the GitHub
+% 
+
+Pt = 'sub-0t3i';
+reconPath = '/MyPC/Documents/Recons'; 
+[num,txt,all]=xlsread(fullfile(reconPath, Pt, [Pt '_RAS.xlsx'] ) );
 
 %Number of RAS Coords
 RAS_coords=[];
@@ -10,54 +18,54 @@ RAS_coords=num(1:length(num),:);
 RAS_labels={};
 RAS_labels=txt(1:length(txt),1);
 
+% Adds the customized snap function from GitHub and the iELVis folder
+addpath(genpath('/MyPC/Documents/MATLAB/Recons/Coregistration_Figures_PreOp_PostOp'))
+addpath(genpath('/MyPC/Documents/MATLAB/Recons/CoregCode/iELVis'))
+
 %% Separate each of the grids and strips into different variables
 %G = RAS_coords(1:56,:);%Lateral Temporal Grid
-gr1 = RAS_coords(1:8,:);
-gr2 = RAS_coords(9:16,:);
-gr3 = RAS_coords(17:24,:);
-gr4 = RAS_coords(25:32,:);
+IHA = RAS_coords(1:6,:);
+IHP = RAS_coords(15:18,:);
 
-pgr1 = RAS_coords(33:37,:);
-pgr2 = RAS_coords(38:42,:);
-pgr3 = RAS_coords(43:47,:);
-pgr4 = RAS_coords(48:52,:);
-
-REF = RAS_coords(79:82,:);
+GS = RAS_coords(19:34,:);
+GI1 = RAS_coords(35:42,:);
+GI2 = RAS_coords(43:50,:);
 
 %Depths -- Will have to be adjusted manually below if you wish to move them
-depths = RAS_coords(83:98,:);
+depths = RAS_coords(7:14,:);
 
 
 %% Code for adjusting the locations of the grids and strips
-surface_path = fullfile( '/home/bourbon-the-huckster/Documents/Recons' , Pt, [Pt '_SurferOutput/surf'] );
-% side = 'r';
+surface_path = fullfile( reconPath , Pt, [Pt '_SurferOutput'], 'surf' );
+side = 'r';
 % surftype = 'pial-outer-smoothed';
-[surf.vert surf.tri]=read_surf([surface_path '/rh.pial-outer-smoothed']);
+[surf.vert surf.tri]=read_surf( fullfile( surface_path, [side  'h.pial-outer-smoothed'] ) );
 
-[gr1_coor_snapped,GR_electrode_pairouts] = snap2dural_energy_customized(gr1,surf,nchoosek(1:32,2));
-[gr2_coor_snapped,GR_electrode_pairouts] = snap2dural_energy_customized(gr2,surf);
-[gr3_coor_snapped,GR_electrode_pairouts] = snap2dural_energy_customized(gr3,surf);
-[gr4_coor_snapped,GR_electrode_pairouts] = snap2dural_energy_customized(gr4,surf);
+% We recommend running each of the following lines one at a time
+[gr1_coor_snapped] = snap2dural_energy_customized(IHA,surf);
+[gr2_coor_snapped] = snap2dural_energy_customized(IHP,surf);
+[gr3_coor_snapped] = snap2dural_energy_customized(GS,surf);
+[gr4_coor_snapped] = snap2dural_energy_customized(GI1,surf);
+[gr5_coor_snapped] = snap2dural_energy_customized(GI2,surf);
 
-[pgr1_coor_snapped,GR_electrode_pairouts] = snap2dural_energy_customized(pgr1,surf);%,nchoosek(1:32,2)
-[pgr2_coor_snapped,GR_electrode_pairouts] = snap2dural_energy_customized(pgr2,surf);
-[pgr3_coor_snapped,GR_electrode_pairouts] = snap2dural_energy_customized(pgr3,surf);
-[pgr4_coor_snapped,GR_electrode_pairouts] = snap2dural_energy_customized(pgr4,surf);
-
-[REF_coor_snapped,GR_electrode_pairouts] = snap2dural_energy_customized(REF,surf);
+% Example advanced Usage
+%[REF_coor_snapped,GR_electrode_pairouts] = snap2dural_energy_customized(REF,surf,nchoosek(1:length(REF),2));
 
 %% Compile new coordinates
-RAS_coords_new = [gr1_coor_snapped;gr2_coor_snapped;gr3_coor_snapped;gr4_coor_snapped;pgr1_coor_snapped;...
-    pgr2_coor_snapped; pgr3_coor_snapped; pgr4_coor_snapped; ATS_coor_snapped; PTS_coor_snapped;...
-    AIS_coor_snapped; CIS_coor_snapped; PIS_coor_snapped; REF_coor_snapped; depths ];
-RAS_as_cell = num2cell(RAS_coords_new);
+RAS_coords_new = [gr1_coor_snapped; gr2_coor_snapped; depths; gr3_coor_snapped; gr4_coor_snapped; gr5_coor_snapped];
+
 R = RAS_coords_new(:,1);
 A = RAS_coords_new(:,2);
 S = RAS_coords_new(:,3);
 RAS_Table = table(RAS_labels(2:end), R, A, S);
 
+R = RAS_coords(:,1);
+A = RAS_coords(:,2);
+S = RAS_coords(:,3);
+RAS_Table_Bkp = table(RAS_labels(2:end), R, A, S);
 
 %% Save new coordinates
-writetable( RAS_Table, fullfile( '/home/bourbon-the-huckster/Documents/Recons', Pt, [Pt '_RAS_Snapped.xlsx'] ) )
+writetable( RAS_Table_Bkp, fullfile( reconPath, Pt, [Pt '_RAS_Backup.xlsx'] ) )
+writetable( RAS_Table, fullfile( reconPath, Pt, [Pt '_RAS.xlsx'] ) )
 
         
